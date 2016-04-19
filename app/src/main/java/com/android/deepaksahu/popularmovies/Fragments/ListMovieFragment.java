7316d01667742178619e77953c79efe8c49c16b9
dsahudev <dsahu123456789@gmail.com>
2016-04-20 00:48:51 +0530

    working on listmovie fragment

---
+++ app/src/main/java/com/android/deepaksahu/popularmovies/Fragments/ListMovieFragment.java
@@ -0,0 +1,33 @@
+package com.android.deepaksahu.popularmovies.Fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by deepaksahu on 20/04/16.
+ */
+public class ListMovieFragment extends Fragment {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+
+}
